d9d1b9b38c214e77f5c2679b67fcbc04d44dd32f
joyamadea <amadeajoy@gmail.com>
2020-05-10 01:02:50 +0700

    fixed image source link with new database links

--- application/views/pages/restaurant.php
+++ application/views/pages/restaurant.php
@@ -13,7 +13,7 @@
 		foreach($food as $f){ 
         ?>        
             <div class="card mb-5">
-                <img class="card-img-top" alt="picture of product" src="<?php echo $f->photoLink ?>" width="200px">
+                <img class="card-img-top" alt="picture of product" src="<?php echo site_url('assets/uploads/files/').$f->photoLink; ?>" width="200px">
                 <div class="body m-3">
                     <p class="card-title"><b><?php echo $f->foodName ?></b></p>
                     <p class="card-text"><?php echo $f->desc ?></p>
