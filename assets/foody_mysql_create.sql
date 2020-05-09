781128b016f93937b216bb790b995e3b8d5c6ddf
joyamadea <amadeajoy@gmail.com>
2020-05-10 01:02:24 +0700

    [MODIFY] database

--- assets/foody_mysql_create.sql
+++ assets/foody_mysql_create.sql
@@ -40,7 +40,7 @@ CREATE TABLE `Food` (
 	PRIMARY KEY (`foodID`)
 );
 
-CREATE TABLE `Food Category` (
+CREATE TABLE `FoodCategory` (
 	`categoryID` INT(3) NOT NULL AUTO_INCREMENT,
 	`categoryName` varchar(255) NOT NULL,
 	PRIMARY KEY (`categoryID`)
@@ -69,7 +69,7 @@ CREATE TABLE `Payment` (
 	PRIMARY KEY (`paymentID`)
 );
 
-ALTER TABLE `Food` ADD CONSTRAINT `Food_fk0` FOREIGN KEY (`foodCategory`) REFERENCES `Food Category`(`categoryID`);
+ALTER TABLE `Food` ADD CONSTRAINT `Food_fk0` FOREIGN KEY (`foodCategory`) REFERENCES `FoodCategory`(`categoryID`);
 
 ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_fk0` FOREIGN KEY (`custID`) REFERENCES `User`(`custID`);
 
@@ -80,14 +80,14 @@ ALTER TABLE `Transaction Detail` ADD CONSTRAINT `Transaction Detail_fk0` FOREIGN
 ALTER TABLE `Transaction Detail` ADD CONSTRAINT `Transaction Detail_fk1` FOREIGN KEY (`foodID`) REFERENCES `Food`(`foodID`);
 
 
-INSERT INTO `Food Category`(`categoryName`) VALUES ('Appetizers'), ('Main Courses'), ('Desserts'), ('Snacks'), ('Drinks');
+INSERT INTO `FoodCategory`(`categoryName`) VALUES ('Appetizer'), ('Main Course'), ('Dessert'), ('Snack'), ('Drink');
 
 INSERT INTO `Food` (`foodName`,`foodCategory`,`stock`, `photoLink`,`desc`,`price`)
-VALUES ( 'Mushroom Soup', '1', '20', 'https://www.budgetbytes.com/wp-content/uploads/2017/09/Creamy-Garlic-Mushroom-Soup-spoon.jpg', 'Creamy mushroom soup that will warm your body', '40000'),
-('Chicken Soup', '1', '20', 'https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheese-Chicken-Soup_EXPS_BFBZ19_42311_E01_17_7b.jpg', 'Creamy and cheesy chicken soup that will warm your body', '40000'),
-('Tenderloin Steak', '2', '20', 'https://cdn0-production-assets-kly.akamaized.net/medias/1532254/big/045813900_1489071066-sirloin.jpg', 'Thick, hearty, juicy steak cooked according to your liking', '300000'),
-('Spaghetti Carbonara', '2', '20','https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--1001491_11.jpg?itok=-ns0A_kt', 'Creamy pasta cooked to al dente','70000'),
-('Chocolate lava cake','3','20','https://bakingamoment.com/wp-content/uploads/2018/02/IMG_6207-chocolate-molten-lava-cakes-recipe-square.jpg','Chocolate goodness that will melt in your mouth', '35000'),
-('Gelato ice cream','3', '20','https://media.suara.com/pictures/653x366/2019/06/20/76389-es-krim-neapolitan-shutterstock.jpg','Italian style ice cream (Chocolate, Strawberry, Vanilla flavor)','35000'),
-('Seasoned Fries','4','20','https://www.cookitrealgood.com/wp-content/uploads/2019/11/frozenfrenchfries14.jpg','Seasoned with our specialty herbs and with your choice of dipping sauce', '40000'),
-('Lemonade','5','20','https://images-gmi-pmc.edge-generalmills.com/2586d951-a46a-4091-aec6-eca3adefb409.jpg','Refreshing lemonade, perfect for summer!','28000')
+VALUES ( 'Mushroom Soup', '1', '20', 'Creamy-Garlic-Mushroom-Soup-spoon.jpg', 'Creamy mushroom soup that will warm your body', '40000'),
+('Chicken Soup', '1', '20', 'soup.jpg', 'Creamy and cheesy chicken soup that will warm your body', '40000'),
+('Tenderloin Steak', '2', '20', '045813900_1489071066-sirloin.jpg', 'Thick, hearty, juicy steak cooked according to your liking', '300000'),
+('Spaghetti Carbonara', '2', '20','recipe-image-legacy-id--1001491_11.jpg', 'Creamy pasta cooked to al dente','70000'),
+('Chocolate Lava Cake','3','20','IMG_6207-chocolate-molten-lava-cakes-recipe-square.jpg','Chocolate goodness that will melt in your mouth', '35000'),
+('Gelato Ice Cream','3', '20','76389-es-krim-neapolitan-shutterstock.jpg','Italian style ice cream (Chocolate, Strawberry, Vanilla flavor)','35000'),
+('Seasoned Fries','4','20','frozenfrenchfries14.jpg','Seasoned with our specialty herbs and with your choice of dipping sauce', '40000'),
+('Lemonade','5','20','2586d951-a46a-4091-aec6-eca3adefb409.jpg','Refreshing lemonade, perfect for summer!','28000')
