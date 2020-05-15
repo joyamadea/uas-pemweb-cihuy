7e04327fcd5fd813b0129ac15ec9310d12a834aa
joyamadea <amadeajoy@gmail.com>
2020-05-16 02:45:04 +0700

    Login success with session

--- application/models/M_Account.php
+++ application/models/M_Account.php
@@ -7,4 +7,17 @@
        {
             $this->db->insert('user',$data);
        }
+
+       function login($email){
+          $query = $this->db->get_where('user',array('email'=>$email));
+ 
+         if($query->num_rows() == 1) {
+             //ambil data user berdasar email
+             return $query->result();
+
+         }else{
+               return false;
+         }
+          
+       }
   }
